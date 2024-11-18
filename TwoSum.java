8f4833c9e3448e722ac2eaede5a8e02291c30881
hammoud-abdellah <hammoudabdellah3@gmail.com>
2024-11-18 15:09:44 +0100

    solved two sum problem

---
+++ TwoSum.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numsMap = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numsMap.containsKey(complement)) {
+                return new int[]{numsMap.get(complement), i};
+            } else {
+                numsMap.put(nums[i], i);
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
--- main.java
+++ main.java
@@ -1,24 +1,13 @@
 public class main {
     public static void main(String[] args) {
 
-        ValidAnagram solution = new ValidAnagram();
+        TwoSum solution = new TwoSum();
 
         // Test cases
-        String s1 = "anagram";
-        String t1 = "nagaram";
-
-        // System.out.println(solution.sortString(t1));
-
-        String s2 = "rat";
-        String t2 = "car";
-
-        String s5 = "abcd";
-        String t5 = "dcbae"; // False, different lengths
+        int[] arr = {2,7,11,15};
+        int target = 9;
 
         // Print results
-        System.out.println("Test 1: " + solution.isAnagram(s1, t1)); // Expected: true
-        System.out.println("Test 2: " + solution.isAnagram(s2, t2)); // Expected: false
-        System.out.println("Test 5: " + solution.isAnagram(s5, t5)); // Expected: false
-    
+        System.out.println("Test 1: " + solution.twoSum(arr, target));         
     }
 }
